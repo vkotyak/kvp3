3c612aba0984a10b22d8db3fb1a8e707335ea59a
Vinga <v.kotyak@gmail.com>
2020-03-07 13:25:22 +0200

    9 week

--- 7/model.php
+++ 7/model.php
@@ -2,6 +2,7 @@
 class model {
 
     function getDataPrivat($nbn){
+        
         $pburl = urldecode('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid='.$nbn);
         $res_string = implode(file($pburl));
 
